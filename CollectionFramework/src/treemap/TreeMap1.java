1896e69d58c2262666c66ebaf3ada770025fba41
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-07-16 09:40:53 +0530

    Tree Map

---
+++ CollectionFramework/src/treemap/TreeMap1.java
@@ -0,0 +1,16 @@
+package treemap;
+
+import java.util.TreeMap;
+
+public class TreeMap1 {
+
+	public static void main(String[] args) {
+		TreeMap tm1= new TreeMap ();
+		tm1.put(12, "X");
+		tm1.put(8, "A");
+		tm1.put(15, "Z");
+		
+		System.out.println(tm1);
+	}
+
+}
