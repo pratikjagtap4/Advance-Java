e4c112e7218c6820052abca27760de0903747258
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-07-17 07:52:55 +0530

    Thread.sleep method

--- MultiThreading/src/basics/ThreadExample05Sleep.java
+++ MultiThreading/src/basics/ThreadExample05Sleep.java
@@ -1,11 +1,11 @@
 package basics;
 
-public class ThreadExample05Sleep extends Thread{
-	public void run() {
+public class ThreadExample05Sleep extends Thread {
+	public void run()  {
 		for (int i = 65 ; i<91;i++)
 		{
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(2000);
 			}
 			catch (InterruptedException e){
 				System.out.println(e);
@@ -20,7 +20,18 @@ public class ThreadExample05Sleep extends Thread{
 		t1.start();
 		
 		
-
+		Runnable r2 = new Runnable() {
+			public void run() {
+				for(int i = 1 ; i<= 5; i++) {
+					
+					System.out.println(i);
+				}
+			}
+		};
+		
+		Thread t2 = new Thread(r2);
+		t2.start();
+		
 	}
 
 }
