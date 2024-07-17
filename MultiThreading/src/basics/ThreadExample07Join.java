3748feaa3768d17877309c7b682f3a50ba32ee18
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-07-17 08:11:40 +0530

    join method

--- MultiThreading/src/basics/ThreadExample07Join.java
+++ MultiThreading/src/basics/ThreadExample07Join.java
@@ -5,7 +5,7 @@ public class ThreadExample07Join extends Thread {
 		for(int i =1 ; i<=5;i++)
 		{
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(2000);
 			}
 			catch (InterruptedException e) {
 				
@@ -17,11 +17,20 @@ public class ThreadExample07Join extends Thread {
 
 	public static void main(String[] args) throws InterruptedException {
 		ThreadExample07Join t1 = new ThreadExample07Join();
-		ThreadExample07Join t2 = new ThreadExample07Join();
-		ThreadExample07Join t3 = new ThreadExample07Join();
+		
+		Runnable r1 = new Runnable() {
+			public void run()
+			{
+				System.out.println("Hello World" + " " + Thread.currentThread().getName());
+			}
+		};
+		Thread t2 = new Thread(r1);
+		Thread t3 = new Thread(r1);
+//		ThreadExample07Join t2 = new ThreadExample07Join();
+//		ThreadExample07Join t3 = new ThreadExample07Join();
 
 		t1.start();
-		t1.join();
+		t1.join(2000);
 		t2.start();
 		t3.start();
 		t3.join();
