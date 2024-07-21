8a71b841899332e07ee833ea8f20d67b631fbe82
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-07-21 17:21:28 +0530

    lambda Expression

--- Java8/src/lambdaexp/Ex02CarBrands.java
+++ Java8/src/lambdaexp/Ex02CarBrands.java
@@ -13,9 +13,13 @@ public class Ex02CarBrands {
 		System.out.println(car1.details("Thar", 1700000));
 		
 		Ex2Cars car2 = (name , price) -> "Car name is " + name  + " and price is " + price;
-		System.out.println(car2.details("Onmi" , 200000));
+		String result = car2.details("Onmi" , 200000);
+		System.out.println(result);
 		
-		Ex2Cars car3 = (name , price) -> "Car name is " + name  + " and price is " + price;;
+		Ex2Cars car3 = (name , price) ->{ 
+			System.out.println("Hello");
+			return ("Car name is " + name  + " and price is " + price);
+			};
 		System.out.println(car3.details("nano" , 100000));
 	}	
 
