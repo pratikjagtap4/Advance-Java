1d7bcf4df1ba8a63a7c9b6464d91bd88e771a675
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-08-13 22:50:01 +0530

    Servlet

---
+++ ServletOne/src/main/java/com/firstServlets/_03GenericServlet.java
@@ -0,0 +1,27 @@
+package com.firstServlets;
+
+import java.io.IOException;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet("/_03GenericServlet")
+public class _03GenericServlet extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public _03GenericServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
