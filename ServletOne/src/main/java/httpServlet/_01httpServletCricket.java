050f91bc67e5d4936dbe0e1a068854385af78a98
pratikjagtap4 <npratikjagtap2704@gmail.com>
2024-08-22 22:36:59 +0530

    http servlets

---
+++ ServletOne/src/main/java/httpServlet/_01httpServletCricket.java
@@ -0,0 +1,38 @@
+package httpServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class _01httpServletCricket extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Rcb</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h2>RCB</h2>");
+		out.println("<p><strong>Best IPL team</strong></p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
+
